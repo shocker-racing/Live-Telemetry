5ea08e83512c3f83a4a347fb22b8f52e318ad35c
AlphaRocket <jxdog2005@gmail.com>
2026-03-15 00:23:23 -0500

    FEAT: Update database initialization and docker configuration

--- database/README.md
+++ database/README.md
@@ -1,4 +1,6 @@
 # Getting Started
 
+go to www.docker.com and download docker
+
 1. run `docker-compose up -d` to start the container
 2. run `docker exec -i live-telemetry psql -U postgres < init-db.sql` to initilize the hypertable
--- database/init-db.sql
+++ database/init-db.sql
@@ -1,13 +1,7 @@
 CREATE TABLE engine_telemetry (
     time                TIMESTAMPTZ       NOT NULL,
-    engine_rpm          DOUBLE PRECISION  NULL,
-    engine_oil_temp     DOUBLE PRECISION  NULL,
-    engine_coolant_temp DOUBLE PRECISION  NULL,
-    lambda              DOUBLE PRECISION  NULL,
-    coolant_flow        DOUBLE PRECISION  NULL,
-    oil_pressure        DOUBLE PRECISION  NULL,
-    fuel_pressure       DOUBLE PRECISION  NULL,
-    acceleration        DOUBLE PRECISION  NULL
+    S01PID              DOUBLE PRECISION  NULL,
+    Speed               DOUBLE PRECISION  NULL
 );
 
 SELECT create_hypertable('engine_telemetry', 'time');
