7585d0694f37de05d41cdfbbe2dcedd9bf608c5b
AlphaRocket <jxdog2005@gmail.com>
2026-04-12 00:29:19 -0500

    FEAT: Refactor database schema and add new tables for telemetry, driver, and session data
    Improved data organization and prepared the system for richer analytics

--- database/init-db.sql
+++ database/init-db.sql
@@ -1,9 +1,33 @@
 CREATE EXTENSION IF NOT EXISTS timescaledb;
 
-CREATE TABLE engine_telemetry (
-    time                TIMESTAMPTZ       NOT NULL,
-    Speed               DOUBLE PRECISION  NULL,
-    Acceleration        DOUBLE PRECISION  NULL
+CREATE TABLE driver (
+    driver_id SERIAL PRIMARY KEY,
+    team VARCHAR(255) NOT NULL,
+    driver_number INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    UNIQUE (name, driver_number)
 );
 
-SELECT create_hypertable('engine_telemetry', 'time');
+CREATE TABLE session (
+    session_id SERIAL PRIMARY KEY,
+    driver_id INT NOT NULL,
+    session_date DATE NOT NULL,
+
+    FOREIGN KEY (driver_id) REFERENCES driver(driver_id)
+);
+
+CREATE TABLE telemetry (
+    time TIMESTAMPTZ NOT NULL,
+    speed FLOAT NOT NULL,
+    acceleration FLOAT NOT NULL,
+
+    session_id INT NOT NULL,
+    driver_id INT NOT NULL,
+
+    PRIMARY KEY (session_id, time),
+
+    FOREIGN KEY (session_id) REFERENCES session(session_id),
+    FOREIGN KEY (driver_id) REFERENCES driver(driver_id)
+);
+
+SELECT create_hypertable('telemetry', 'time', if_not_exists => TRUE);
